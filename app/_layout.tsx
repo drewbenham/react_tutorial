daae5c86d2ca22e3bb62098ea093d8f49500ac78
drewbenham <drew_benham@yahoo.com>
2024-10-12 17:27:37 -0700

    Adding navigation tab bar style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { Slot, Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home', headerShown: true}} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   )
 }
