617906bd53743450d9400a7f06a3a9e26fddf4cd
drewbenham <drew_benham@yahoo.com>
2024-10-14 19:04:53 -0700

    select an image from device.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@/components/Button'
 import ImageViewer from '@/components/ImageViewer';
 import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
 
 
 const PlaceholderImage =  require('../../assets/images/icon.png')
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image.')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label='Choose a Photo' />
+        <Button theme="primary" label='Choose a Photo' onPress={pickImageAsync} />
         <Button label='Use this Photo' />
       </View>
       <StatusBar style='auto' />
